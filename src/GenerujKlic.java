493fbd692ad80f40ab7087fb754d8396afc6045d
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2023-11-06 17:47:34 +0100

    Initial commit

---
+++ src/GenerujKlic.java
@@ -0,0 +1,23 @@
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class GenerujKlic {
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128);
+        SecretKey key = keyGenerator.generateKey();
+
+        byte[] k = key.getEncoded();
+
+        Path jmenoSouboru = Paths.get("mujklic.dat");
+        Files.write(jmenoSouboru, k);
+
+        System.out.println(Arrays.toString(k));
+    }
+}
