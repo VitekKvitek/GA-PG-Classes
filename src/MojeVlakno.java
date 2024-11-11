8c9ff64aaf055daec81439bdc03e1a9e063f099c
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2024-11-11 09:10:46 +0100

    anonymni metody a lambda expression

---
+++ src/MojeVlakno.java
@@ -0,0 +1,34 @@
+public class MojeVlakno implements Runnable{
+
+    public void run() {
+        for(int i = 0; i<100; i++)
+            System.out.println(i);
+    }
+    public static void main(String[] args) {
+        Runnable v1 = new MojeVlakno();
+        Thread t1 = new Thread(v1);
+
+        //nemuze mit constructor
+        // anonymni trida
+        Runnable v2 = new Runnable(){
+            public void run() {
+                for(int i = 'a'; i<'z'; i++)
+                    System.out.println((char)i);
+            }
+        };
+        Thread t2 = new Thread(v2);
+        // lambda expression
+        Runnable v3 = ()->{
+            // Runnable ma jen jednu metodu
+            //takze staci napsat jen telo metody
+            {
+                for(int i = -1; i>-100; i--)
+                    System.out.println(i);
+            }
+        };
+        Thread t3 = new Thread(v3);
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
--- src/Trojice.java
+++ src/Trojice.java
@@ -2,43 +2,43 @@ import java.util.Iterator;
 // implements Iterable je extra, nahrada za MujIterator
 //musime mit metodu iterator
 //ta vrati objekt co implementuje Iterator
-public class Trojice implements Iterable<Integer>{ // jaky typ to ma vracet
-    int a,b,c;
-    int index = 0;
-    public boolean hasNext(){
-        return index <= 2;
-    }
-    public int next(){
-        switch (index++){
-            case 0: return a;
-            case 1: return b;
-            default: return c;
-        }
-    }
+public class Trojice implements Iterable<Integer> { // jaky typ to ma vracet
+
+    int a, b, c;
+
     public Trojice(int a, int b, int c) {
         this.a = a;
         this.b = b;
         this.c = c;
     }
+
     // kdyz implemetns Iterable, musi mit tuhle funkci
-    public MujIterator iterator(){
-        return new MujIterator(this);
+    public Iterator<Integer> iterator() {
+        //return new InterniIterator();
+
+        return new Iterator<Integer>() {
+            int index = 0;
+
+            public boolean hasNext() {
+                return index < 3;
+            }
+
+            public Integer next() { // jaky typ to ma vracet
+                switch (index++) {
+                    case 0:
+                        return a;
+                    case 1:
+                        return b;
+                    default:
+                        return c;
+                }
+            }
+        };
     }
-    // nase fake custom funkce - takhle ne
-    public MujIterator dejMiIteraotr(){
-        return new MujIterator(this);
-    }
+
     public static void main(String[] args) {
         Trojice t = new Trojice(3,5,7);
-        MujIterator it = t.dejMiIteraotr();
-        while (it.hasNext()){
-            System.out.println(it.next());
-        }
-        // nelze pres stejny iterator iterovat znova
-        // musel by se vytvorit novy
-        while (it.hasNext()){
-            System.out.println(it.next());
-        }
+
         // iterator je basically for each - je to zabudovane v Jave
         // dvojice
         // random note - 4 objekty iteratoru
